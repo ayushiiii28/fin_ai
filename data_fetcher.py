c0749b95fd2c5ff104f64c09a1559f984ab6dd48
Ayushi <139673586+ayushiiii28@users.noreply.github.com>
2025-08-05 19:25:57 +0530

    Update data_fetcher.py

--- data_fetcher.py
+++ data_fetcher.py
@@ -1,5 +1,5 @@
 import yfinance as yf
 
 def get_stock_data(ticker, period="1mo", interval="1d"):
-    data = yf.download(ticker, period=period, interval=interval, progress=False)
+    data = yf.download(ticker, period=period, interval=interval, progress=False, auto_adjust=True)
     return data[['Close']]
