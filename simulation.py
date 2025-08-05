07f233ac7e1beaf6bf0151b2a4ef43550af9eb88
Ayushi <139673586+ayushiiii28@users.noreply.github.com>
2025-08-05 19:53:47 +0530

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -3,25 +3,14 @@ from predictor import predict_next_close
 from allocator import allocate_funds
 
 def simulate_investment(total_investment, assets):
-    prediction_scores = {}
-    details = {}
-
+    predictions = {}
     for asset in assets:
         df = get_stock_data(asset)
-        if df.empty:
+        if df.empty or len(df) < 10:
             continue
         predicted_close = predict_next_close(df['Close'].values)
         last_close = df['Close'].values[-1]
-
         expected_return = (predicted_close - last_close) / last_close
-
-        prediction_scores[asset] = expected_return
-        details[asset] = {
-            "predicted_close": round(float(predicted_close), 2),
-            "last_close": round(float(last_close), 2),
-            "expected_return": round(float(expected_return) * 100, 2)
-        }
-
-
-    allocations = allocate_funds(prediction_scores, total_investment)
-    return allocations, details
+        predictions[asset] = expected_return
+    allocations = allocate_funds(predictions, total_investment)
+    return allocations, predictions
