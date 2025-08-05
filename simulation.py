2aaea9c61448c1f15e4a36013ff316c857c8d1e6
Ayushi <139673586+ayushiiii28@users.noreply.github.com>
2025-08-05 19:06:55 +0530

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -15,12 +15,13 @@ def simulate_investment(total_investment, assets):
 
         expected_return = (predicted_close - last_close) / last_close
 
-        prediction_scores[asset] = expected_return   # only the score goes to allocator
+        prediction_scores[asset] = expected_return
         details[asset] = {
-            "predicted_close": round(predicted_close, 2),
-            "last_close": round(last_close, 2),
-            "expected_return": round(expected_return * 100, 2)
+            "predicted_close": round(float(predicted_close), 2),
+            "last_close": round(float(last_close), 2),
+            "expected_return": round(float(expected_return) * 100, 2)
         }
+
 
     allocations = allocate_funds(prediction_scores, total_investment)
     return allocations, details
